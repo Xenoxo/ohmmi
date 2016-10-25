ff72dcf7861b55df689faa63be21f078cc60d53a
Xenoxo <timwyc@gmail.com>
2016-10-25 16:29:09 +0200

    Make conditional expansion work

--- Instructions.js
+++ Instructions.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, TouchableOpacity, ScrollView, View, Text, LayoutAnimation, UIManager } from 'react-native';
+import { StyleSheet, TouchableOpacity, ScrollView, View, Text, LayoutAnimation, UIManager, Touc } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import update from 'immutability-helper';
 const styles = StyleSheet.create({
@@ -44,12 +44,7 @@ export default class Instructions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      t: {
-        text0: 0,
-        text1: 0,
-        text2: 0,
-      },
-      h: [0,0,0]
+      h: [0,0,0,0,0,0,0]
     }
     this.popit = this.popit.bind(this);
     UIManager.setLayoutAnimationEnabledExperimental && 
@@ -57,8 +52,6 @@ export default class Instructions extends Component {
   }
 
   componentWillMount() {
-    console.log("on will mount "+this.state.h)
-    // Animate creation
     LayoutAnimation.spring();
   }
 
@@ -68,37 +61,11 @@ export default class Instructions extends Component {
 
   onPressAnimation(index) {
     LayoutAnimation.spring();
-    // console.log(this.state.t[0])
-    // let newarr = this.state.h.slice()
-    // console.log("this is newarr "+newarr)
-    
-    // newarr[index] = ((this.state.h[index] - 50) * -1);
-    // newarr[index] = 50;
-
-    // console.log("this is new newarr "+newarr)
-    // this.setState({ h:newarr });
-    // console.log("this is new h "+this.state.h)
     if (index !== -1) {
-      // let newArray = this.state.h.slice();
-      // let num =  this.state.h[index];
-      
-      // newArray[index] = ((num - 50) * -1);
-      // this.setState({ h:newArray });
-
-      // const collection = this.state.h.splice()
-      console.log("what is here at index 0? " + this.state.h[index]);
-      
-      // let num = (this.state.h[index] - 50) * -1;
-      
-      // console.log("num " + num)
       let updateQuery = {}
-      updateQuery[index] = { $set: (this.state.h[0] + 1) * -1 };
-
+      updateQuery[index] = { $set: (this.state.h[index] + 1) * -1 };
       const newCollection = update(this.state.h, updateQuery);
-      
       this.setState({h:newCollection})
-      
-      console.log(this.state.h)
     }
   }
 
@@ -107,39 +74,56 @@ export default class Instructions extends Component {
       <View style={styles.container}>
         <ScrollView contentContainerStyle={{ marginLeft: 16, marginRight: 16 }}>
           <Text style={[styles.header, { fontSize: 40, color: '#8BC34A' }]}>How to Meditate</Text>
-
-          <Text
-            style={styles.header}
-            onPress={this.onPressAnimation.bind(this, 0)}
-          >Find a comfortable seated position.</Text>
-          <Text style={[styles.text, { height: this.state.h[0] }]}>
-          You can sit in a chair, on a cushion, or just on the ground. The idea is to be comfortable but not enough where you are falling asleep.
-          </Text>
           
-          <Text style={styles.header}>Keep your back straight.</Text>
-          <Text style={styles.text}>
-          Inhale, roll the shoulders up to your ears. Exhale, roll them back and down. This positions the head atop your neck while floating the shoulders over the hips.{"\n"}{"\n"}
-          Consider this a neutral, tall spine. When you feel yourself hunching or slumping, reset your position with this method.
-          </Text>
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 0)}>
+            <Text style={styles.header}>Find a comfortable seated position.</Text>
+            <Text style={[styles.text, { height: this.state.h[0] }]}>
+              You can sit in a chair, on a cushion, or just on the ground. The idea is to be comfortable but not enough where you are falling asleep.
+            </Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 1)}>
+            <Text style={styles.header}>Keep your back straight.</Text>
+            <Text style={[styles.text, { height: this.state.h[1] }]}>
+              Inhale, roll the shoulders up to your ears. Exhale, roll them back and down. This positions the head atop your neck while floating the shoulders over the hips.{"\n"}{"\n"}
+              Consider this a neutral, tall spine. When you feel yourself hunching or slumping, reset your position with this method.
+            </Text>
+          </TouchableOpacity>
 
-          <Text style={styles.header}>Close your eyes.</Text>
-          <Text style={styles.text}>Gently close your eyes, the idea is to block out potential visual stimuli.</Text>
-          <Text style={styles.header}>Maintain a simple breath.</Text>
-          <Text style={styles.text}>Try for a steady deep breath. Nasal breathing makes it easier to find a smooth, even pace.</Text>
-          <Text style={styles.header}>Focus on breathing.</Text>
-          <Text style={styles.text}>Focus solely on the feeling of the air entering and exiting your nostrils.</Text>
-          <Text style={styles.header}>Maintain focus.</Text>
-          <Text style={styles.text}>
-            Your mind will naturally wander and thoughts will bubble into your consciousness. This is expected.{"\n"}{"\n"}
-            When you realize awareness has drifted, acknowledge the thoughts, let them pass as you gently guide your focus back to the breath.
-          </Text>
-          <Text style={styles.header}>Give it time.</Text>
-          <Text style={styles.text}>
-            Meditation takes practice and at times can be frustrating trying to maintain focus. However continual practice will substantially help in this regard.{"\n"}{"\n"}
-            A session of 10 minutes a day has shown to improve many facets of mental and physical well being when done consistently.
-          </Text>
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 2)}>
+            <Text style={styles.header}>Close your eyes.</Text>
+            <Text style={[styles.text, { height: this.state.h[2] }]}>Gently close your eyes, the idea is to block out potential visual stimuli.</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 3)}>
+            <Text style={styles.header}>Maintain a simple breath.</Text>
+            <Text style={[styles.text, { height: this.state.h[3] }]}>Try for a steady deep breath. Nasal breathing makes it easier to find a smooth, even pace.</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 4)}>
+            <Text style={styles.header}>Focus on breathing.</Text>
+            <Text style={[styles.text, { height: this.state.h[4] }]}>Focus solely on the feeling of the air entering and exiting your nostrils.</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 5)}>           
+            <Text style={styles.header}>Maintain focus.</Text>
+            <Text style={[styles.text, { height: this.state.h[5] }]}>
+              Your mind will naturally wander and thoughts will bubble into your consciousness. This is expected.{"\n"}{"\n"}
+              When you realize awareness has drifted, acknowledge the thoughts, let them pass as you gently guide your focus back to the breath.
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 6)}>
+            <Text style={styles.header}>Give it time.</Text>
+            <Text style={[styles.text, { height: this.state.h[6] }]}>
+              Meditation takes practice and at times can be frustrating trying to maintain focus. However continual practice will substantially help in this regard.{"\n"}{"\n"}
+              A session of 10 minutes a day has shown to improve many facets of mental and physical well being when done consistently.
+            </Text>
+          </TouchableOpacity>
+
           <Text style={[styles.text, { marginTop: 15, marginBottom: 15, textAlign: 'center', fontWeight: '900' }]}>You can do it!</Text>
         </ScrollView>
+
         <View style={{ backgroundColor: '#F5F5F5', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', borderTopWidth: 1, borderColor: '#E0E0E0' }}>
           <TouchableOpacity onPress={this.popit} >
             <View style={[styles.smallCircleButton, { backgroundColor: '#F8BBD0' }]}>
