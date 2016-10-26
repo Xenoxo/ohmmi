2a94c12f0c384efa8140dae9b8a663730903ef33
Xenoxo <timwyc@gmail.com>
2016-10-26 16:08:26 +0200

    Add all instructions to as a component. Apply various refactoring fixes.

--- InstructionStep.js
+++ InstructionStep.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, TouchableOpacity, ScrollView, View, Text, LayoutAnimation, UIManager, Touc } from 'react-native';
+import { TouchableOpacity, ScrollView, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class InstructionStep extends Component {
@@ -7,55 +7,15 @@ export default class InstructionStep extends Component {
 		return (
 			<View>
 	      <TouchableOpacity onPress={this.props.onPressHandler}>
-	        <View style={{ flex:1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginRight:16 }}>
-	          <Text style={styles.header}>Find comfortable seating</Text>
+	        <View style={this.props.headerContainerStyle}>
+	          <Text style={this.props.headerStyle}>{ this.props.header }</Text>
 	          <Icon style={this.props.iconStyle} name="chevron-right" size={20} />
 	        </View>
 	      </TouchableOpacity>  
         <Text style={this.props.textStyle}>
-          You can sit in a chair, on a cushion, or just on the ground.{"\n"}{"\n"}
-          The idea is to be comfortable but not enough where you are falling asleep.
+        	{ this.props.text }
         </Text>
        </View>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
-
-  },
-  buttonContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  smallCircleButton: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 45,
-    height: 45,
-    borderRadius: 50,
-    marginTop: 15,
-    marginBottom: 22,
-    elevation: 4,
-  },
-  header: {
-    fontSize: 20,
-    fontWeight: '900',
-    marginBottom: 5,
-    opacity: 1,
-    color: '#424242',
-  },
-  text: {
-    fontSize: 15,
-    marginBottom: 10,
-    marginLeft: 16,
-    color: '#424242',
-    opacity: 0.75,
-  },
-});
--- Instructions.js
+++ Instructions.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, TouchableOpacity, ScrollView, View, Text, LayoutAnimation, UIManager, Touc } from 'react-native';
+import { StyleSheet, TouchableOpacity, ScrollView, View, Text, LayoutAnimation, UIManager } from 'react-native';
 import InstructionStep from './InstructionStep.js';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import update from 'immutability-helper';
@@ -26,6 +26,13 @@ const styles = StyleSheet.create({
     marginBottom: 22,
     elevation: 4,
   },
+  headerContainer: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight:16,
+  },
   header: {
     fontSize: 20,
     fontWeight: '900',
@@ -51,7 +58,6 @@ export default class Instructions extends Component {
     this.popit = this.popit.bind(this);
     UIManager.setLayoutAnimationEnabledExperimental && 
     UIManager.setLayoutAnimationEnabledExperimental(true);
-    
   }
 
   componentWillMount() {
@@ -79,54 +85,77 @@ export default class Instructions extends Component {
         <ScrollView contentContainerStyle={{ marginLeft: 16, marginRight: 16}}>
           <View style={{ marginBottom: 10, }}>
             <Text style={[styles.header, { fontSize: 40, color: '#8BC34A', marginBottom: 0, }]}>How to Meditate</Text>
-            <Text style={{ fontSize: 10, color: '#424242', opacity: 0.9, }}>(tap on the headers for more information)</Text>
           </View>
 
-          <InstructionStep 
+          <InstructionStep
             onPressHandler={this.onPressAnimation.bind(this, 0)}
             iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[0] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
             textStyle={[styles.text, { height: this.state.h[0] }]}
+            header={'Find comfortable seating'}
+            text={'You can sit in a chair, on a cushion, or just on the ground.\n \nThe idea is to be comfortable but not enough where you are falling asleep.'}
           />
           
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 1)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[1] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[1] }]}
+            header={'Keep your back straight'}
+            text={'Inhale, roll the shoulders up to your ears. Exhale, roll them back and down. This positions the head atop your neck while floating the shoulders over the hips.\n\nConsider this a neutral, tall spine. When you feel yourself hunching or slumping, reset your position with this method.'}
+          />
+
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 2)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[2] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[2] }]}
+            header={'Close your eyes'}
+            text={'Gently close your eyes, the idea is to block out potential visual stimuli.'}
+          />
+
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 3)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[3] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[3] }]}
+            header={'Maintain a simple breath'}
+            text={'Try for a steady deep breath. Nasal breathing makes it easier to find a smooth, even pace.'}
+          />
           
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 1)}>
-            <Text style={styles.header}>Keep your back straight.</Text>
-            <Text style={[styles.text, { height: this.state.h[1] }]}>
-              Inhale, roll the shoulders up to your ears. Exhale, roll them back and down. This positions the head atop your neck while floating the shoulders over the hips.{"\n"}{"\n"}
-              Consider this a neutral, tall spine. When you feel yourself hunching or slumping, reset your position with this method.
-            </Text>
-          </TouchableOpacity>
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 4)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[4] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[4] }]}
+            header={'Focus on breathing'}
+            text={'Focus solely on the feeling of the air entering and exiting your nostrils.'}
+          />
 
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 2)}>
-            <Text style={styles.header}>Close your eyes.</Text>
-            <Text style={[styles.text, { height: this.state.h[2] }]}>Gently close your eyes, the idea is to block out potential visual stimuli.</Text>
-          </TouchableOpacity>
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 5)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[5] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[5] }]}
+            header={'Refocus when distracted'}
+            text={'Your mind will naturally wander and thoughts will bubble into your consciousness. This is expected.\n\nWhen you realize awareness has drifted, acknowledge the thoughts, let them pass as you gently guide your focus back to the breath.'}
+          />
 
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 3)}>
-            <Text style={styles.header}>Maintain a simple breath.</Text>
-            <Text style={[styles.text, { height: this.state.h[3] }]}>Try for a steady deep breath. Nasal breathing makes it easier to find a smooth, even pace.</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 4)}>
-            <Text style={styles.header}>Focus on breathing.</Text>
-            <Text style={[styles.text, { height: this.state.h[4] }]}>Focus solely on the feeling of the air entering and exiting your nostrils.</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 5)}>           
-            <Text style={styles.header}>When distracted, refocus.</Text>
-            <Text style={[styles.text, { height: this.state.h[5] }]}>
-              Your mind will naturally wander and thoughts will bubble into your consciousness. This is expected.{"\n"}{"\n"}
-              When you realize awareness has drifted, acknowledge the thoughts, let them pass as you gently guide your focus back to the breath.
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={this.onPressAnimation.bind(this, 6)}>
-            <Text style={styles.header}>Give it time.</Text>
-            <Text style={[styles.text, { height: this.state.h[6] }]}>
-              Meditation takes practice and at times can be frustrating trying to maintain focus. However, continual practice will substantially help in this regard.{"\n"}{"\n"}
-              A session of 10 minutes a day has shown to improve many facets of mental and physical well being when done consistently.
-            </Text>
-          </TouchableOpacity>
+          <InstructionStep
+            onPressHandler={this.onPressAnimation.bind(this, 6)}
+            iconStyle={{ color: '#424242', transform:[{ rotate: (this.state.h[6] * -90) + ' deg' }]}}
+            headerContainerStyle={styles.headerContainer}
+            headerStyle={styles.header}
+            textStyle={[styles.text, { height: this.state.h[6] }]}
+            header={'Give it time'}
+            text={'Meditation takes practice and at times can be frustrating trying to maintain focus. However, continual practice will substantially help in this regard.\n\nA session of just 5 minutes a day has shown to improve many facets of mental and physical well being when done consistently.'}
+          />
         </ScrollView>
 
         <View style={{ backgroundColor: '#F5F5F5', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center', borderTopWidth: 1, borderColor: '#E0E0E0' }}>
